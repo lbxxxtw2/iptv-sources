c63ea0d428778900cbfa7d775b3b4cd20f6aa5d5
lbxxxtw <43683467+lbxxxtw2@users.noreply.github.com>
2025-02-17 17:05:31 +0800

    Update yuechan_live.ts

--- src/sources/yuechan_live.ts
+++ src/sources/yuechan_live.ts
@@ -21,7 +21,7 @@ export const yuechan_live_sources: TSources = [
     {
         name: "YueChan-Live IPTV",
         f_name: "ycl_iptv",
-        url: "https://raw.githubusercontent.com/YueChan/Live/main/IPTV.m3u",
+        url: "https://www.stream-link.org/playlist.m3u?token=e791cb61-c75c-4eae-bf4a-f60b73a1aaf3&hmac=3445ba3bf254b7aa27cc23df0705bb2b",
         filter: yuechan_live_filter,
     },
 ]
